2becab36a37d6f96a761a0eaf5c1eea267b21fc5
msjun23 <msjun23@gmail.com>
2020-08-22 19:08:09 +0900

    error: no lane detected
    lane_detection_test1.py: can't detect lanes.
    Hough Space 변환에서 문제가 있는 듯 하다.
    그 이전 Region Of Interest 설정까지는 잘 됨

---
+++ houghlinesp_test.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("images/building.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,50,150,apertureSize = 3)
+minLineLength = 100
+maxLineGap = 50
+lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+for x1,y1,x2,y2 in lines[0]:
+    cv2.line(img,(x1,y1),(x2,y2),(255,0,0),2)
+
+cv2.imshow("img", img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
