62d84a9052ace24791eb10aaa4c85f84841cf67c
DESKTOP-FFB0ERD\msjun_msi <msjun23@gmail.com>
2020-08-25 09:27:45 +0900

    edit functions
    edit utils/ImagePreprocessing.py

--- utils/ImagePreprocessing.py
+++ utils/ImagePreprocessing.py
@@ -2,14 +2,27 @@ import cv2
 import numpy as np
 
 def ImagePreprocessing(img):
+	## masking image (yellow and white)
+	mask_y_w_img = MaskImage(img)
 
+	## set ROI-Region Of Interest
+	roi_img = SetROI(mask_y_w_img)
+
+	## Canny Edge Detection
+	canny_edges = GetCannyEdge(roi_img)
+
+	## Hough Transform
+	res = HoughTransform(canny_edges, img)
+
+	return res
+	
+
+def MaskImage(img):
+	## rgb -> hsv
+	hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 	## rgb -> gray
 	gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	## rgb -> hsv
-	hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-
-	## masking image (yellow and white)
 	lower_yellow = np.array([20,100,100], dtype="uint8")
 	upper_yellow = np.array([30,255,255], dtype="uint8")
 
@@ -18,12 +31,14 @@ def ImagePreprocessing(img):
 	mask_y_w = cv2.bitwise_or(mask_yellow, mask_white)
 	mask_y_w_img = cv2.bitwise_and(gray_img, mask_y_w)
 
+	return mask_y_w_img
 
-	## set ROI-Region Of Interest
-	mask = np.zeros_like(mask_y_w_img)
+
+def SetROI(img):
+	mask = np.zeros_like(img)
 	ignore_color = 255
 
-	imshape = mask_y_w_img.shape
+	imshape = img.shape
 	lower_left = [imshape[1]/9,imshape[0]]
 	lower_right = [imshape[1]-imshape[1]/9,imshape[0]]
 	top_left = [imshape[1]/2-imshape[1]/8,imshape[0]/2+imshape[0]/10]
@@ -32,36 +47,41 @@ def ImagePreprocessing(img):
 
 	cv2.fillPoly(mask, vertices, ignore_color)
 
-	roi_img = cv2.bitwise_and(mask_y_w_img, mask)
+	roi_img = cv2.bitwise_and(img, mask)
+	return roi_img
 
 
+def GetCannyEdge(img):
 	## apply Gaussian blur to suppress noise in Canny Edge Detection
 	kernel_size = (5,5)
-	gaussian_img = cv2.GaussianBlur(roi_img, kernel_size, 0)
+	gaussian_img = cv2.GaussianBlur(img, kernel_size, 0)
 
-
-	## Canny Edge Detection
 	low_threshold = 50
 	upper_threshold = 150
 	canny_edges = cv2.Canny(gaussian_img, low_threshold, upper_threshold)
 
+	return canny_edges
 
-	## Hough Space
-	# use cv2.HoughLinesP
+
+def HoughTransform(img, res):
+	## use cv2.HoughLinesP
 	hough_threshold = 10
 	minLineLength = 1
 	maxLineGap = 20
 
-	lines = cv2.HoughLinesP(canny_edges, 1, np.pi/180, threshold=hough_threshold, minLineLength=minLineLength, maxLineGap=maxLineGap)
+	lines = cv2.HoughLinesP(img, 1, np.pi/180, threshold=hough_threshold, minLineLength=minLineLength, maxLineGap=maxLineGap)
 
+	DrawLines(res, lines)
+
+	return res
+
+
+def DrawLines(img, lines, color=[0,0,255], thickness=2):
 	for line in lines:
 		x1 = line[0][0]
 		y1 = line[0][1]
 		x2 = line[0][2]
 		y2 = line[0][3]
 
-		cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 2)
-
-
-	return img
-	
+		cv2.line(img, (x1,y1), (x2,y2), color, thickness)
+		
